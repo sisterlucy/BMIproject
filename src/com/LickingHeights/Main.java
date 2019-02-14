8802c0b257b3053e92e293e85ad30929cc56297d
Unknown <lucy.kasler.2022@student.lhschools.org>
2019-02-14 13:23:43 -0500

    Asked for measurements

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -7,10 +7,31 @@ public class Main {
     public static void main(String[] args) {
        String inches;
        String pounds;
+       String userName;
+       String one;
+       String two;
+       String three;
+        //BMI= kg/m*m
+        // m= inch* (0.0254/1)
+        //inch= m*(1/0.0254)
+
        Scanner keyboard;
 
        keyboard= new Scanner(System.in);
+
+       System.out.println("Height (feet only)");
+       one = keyboard.nextLine();
+       System.out.println("height (inches only)");
+       two = keyboard.nextLine();
+       System.out.println("Weight (in pounds)");
+       three =keyboard.nextLine();
        
+
+
+
+
+
+
 
 
 	
